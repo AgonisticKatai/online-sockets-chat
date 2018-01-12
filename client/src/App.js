47b40a78318f7d556efa4bc6a217fd40200b9ec3
Nacho Torrella <nachotorrella@gmail.com>
2018-01-12 14:57:44 +0100

    added input form

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import InputForm from "components/InputForm/Inputform";
+import MessagePanel from "components/MessagePanel/MessagePanel";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Online Sockets Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <InputForm />
+        <MessagePanel />
       </div>
     );
   }
---
+++ client/src/components/InputForm/Inputform.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const InputForm = props => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const newMessage = {
+      message: this.messageInput.value
+    };
+    this.messageInput.value = "";
+  };
+
+  return (
+    <div className="d-flex justify-content-center my-5">
+      <form className="form-row align-items-center" onSubmit={handleSubmit}>
+        <div className="col-auto">
+          <input
+            type="text"
+            className="form-control mb-2"
+            placeholder="Enter new message..."
+            name="taskInput"
+            autoFocus
+            required={true}
+            ref={node => {
+              this.messageInput = node;
+            }}
+          />
+        </div>
+        <div className="col-auto">
+          <button type="submit" className="btn btn-primary mb-2">
+            Send message
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default InputForm;
