d784f51eaf8cc4e45af784a46774d0cb76d0e5ee
Nacho Torrella <nachotorrella@gmail.com>
2018-01-15 11:17:23 +0100

    edit message container

--- client/src/components/MessagePanel/MessagePanel.js
+++ client/src/components/MessagePanel/MessagePanel.js
@@ -5,17 +5,9 @@ const MessagePanel = props => {
     return (
       <div key={index} className="d-flex justify-content-center">
         <div className="list-group">
-          <a
-            href="/"
-            className="list-group-item list-group-item-action flex-column align-items-start"
-          >
-            <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">{message.socket}</h5>
-              <small>3 days ago</small>
-            </div>
-            <p className="mb-1">{message.message}</p>
-            <small>Donec id elit non mi porta.</small>
-          </a>
+          <div className="d-flex w-100 justify-content-between">
+            <p className="mb-1">message: {message.message}</p>
+          </div>
         </div>
       </div>
     );
